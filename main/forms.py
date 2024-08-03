5074dd1694297ad3a74b97f2a8698c632888a063
Olesya Sokolova <o.sokolova@astondevs.ru>
2024-08-03 23:56:18 +0300

    Ti popa

--- main/forms.py
+++ main/forms.py
@@ -1,1 +1,20 @@
+from django import forms
+from .models import Application
 
+
+class ApplicationForm(forms.ModelForm):
+    class Meta:
+        model = Application
+        fields = [
+            "status",
+            "customer_id",
+            "executor_id",
+            "initial_sum",
+            "receiver_id",
+            "sender_id",
+            "executor_commission",
+            "giving_side",
+            "commission_with_interest",
+            "comment",
+            "is_documents",
+        ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.shortcuts import render, redirect
 
+from .forms import ApplicationForm
+
 
 def home(request):
     return render(request, "main/home.html")
@@ -23,3 +25,15 @@ def sign_up(request):
         form = UserCreationForm()
 
     return render(request, "registration/sign_up.html", {"form": form})
+
+
+def create_application(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("success_url")
+    else:
+        form = ApplicationForm()
+
+    return render(request, "application_form.html", {"form": form})
